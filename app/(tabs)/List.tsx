708ac4851834340fe9d65de2b9be59a6a4b4a848
PrayandCray <104704414+PrayandCray@users.noreply.github.com>
2025-05-19 15:42:21 -0400

    Viewable Lists!
    created buttons that change the list value (controls what list is being viewed atm)

--- app/(tabs)/List.tsx
+++ app/(tabs)/List.tsx
@@ -1,8 +1,29 @@
 import React from 'react';
 import AppButton from "@/components/button";
-import {SafeAreaView, Text, StyleSheet} from 'react-native';
+import {SafeAreaView, View, Text, StyleSheet} from 'react-native';
 
 const List = () => {
+    const [activelist, setActiveList] = React.useState<'first' | 'second'>('first');
+
+    const renderListContent = () => {
+        if (activelist === 'first') {
+            return (
+                <View style={styles.listContainer}>
+                    <Text style={styles.listExampleText}>My Pantry</Text>
+                    {/*insert list items here*/}
+                </View>
+            );
+        } else {
+            return (
+                <View style={styles.listContainer}>
+                    <Text style={styles.listExampleText}>Grocery List</Text>
+                    {/*insert list items here*/}
+                </View>
+
+            )
+        }
+    }
+
     return (
         <SafeAreaView style={styles.container}>
             <Text style={styles.title}>
@@ -11,11 +32,33 @@ const List = () => {
             <Text style={styles.subtitle}>
                 This is where your lists are stored
             </Text>
+            <View style={styles.buttonContainer}>
+                <AppButton
+                    text="My Pantry"
+                    onPress={() => setActiveList('first')}
+                    width={150}
+                    borderPadding={20}
+                    borderColor={'#fff'}
+                    textColor={'#EADDCA'}
+                />
+
+                <AppButton
+                    text='Grocery List'
+                    onPress={() => setActiveList('second')}
+                    width={150}
+                    borderPadding={10}
+                    borderColor={activelist === 'first' ? '#b45309' : '#fff'}
+                    textColor={'#EADDCA'}
+                />
+            </View>
+
+            {renderListContent()}
 
             <AppButton
-                text="Add List"
-                onPress={() =>console.log('pressed')}
-                isFullWidth={true} // this is for auto-padding, otherwise specify width with width{number}
+                text="Add Item"
+                onPress={() => console.log('pressed')}
+                isFullWidth={false}
+                width={85}
                 borderPadding={20}
                 borderColor={'#fff'}
                 textColor={'#EADDCA'}
@@ -35,6 +78,27 @@ const styles = StyleSheet.create({
         padding: 16,
         gap: 16,
         width: '100%',
+    },
+    listContainer: {
+        flex: 1,
+        width: '100%',
+        backgroundColor: '#fff',
+        borderRadius: 10,
+        padding: 16,
+        marginVertical: 16,
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        gap: 10,
+        width: '100%',
+        paddingHorizontal: 10,
+    },
+    listExampleText: {
+        color: '#b45309',
+        fontSize: 16,
+        fontWeight: 'bold',
+        textAlign: 'center',
     },
     title: {
         paddingTop: 30,
