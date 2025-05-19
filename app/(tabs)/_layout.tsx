508f7856bdf8ca42982ed4061f0a24369743c15f
PrayandCray <104704414+PrayandCray@users.noreply.github.com>
2025-05-19 09:37:46 -0400

    Changed tab bar icons
    edited the layout.tsx file in the tabs folder to search the routes and set tab names, colors and sizes

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,15 +1,50 @@
 import {Tabs} from "expo-router";
+import { Ionicons} from "@expo/vector-icons";
 
 export default function _layout() {
     return (
         <Tabs
-            screenOptions={{
+            screenOptions={({ route }) => ({
                 headerShown: false,
                 tabBarStyle: {
                     paddingTop: 10,
                     backgroundColor: '#EADDCA'
+                },
+                tabBarIcon: ({ focused, color, size }) => {
+                    switch (route.name) {
+                        case 'Home':
+                            return <Ionicons 
+                                name={focused ? 'home' : 'home-outline'}
+                                size={size} 
+                                color={color}
+                            />;
+                        case 'List':
+                            return <Ionicons 
+                                name={focused ? 'list' : 'list-outline'} 
+                                size={size} 
+                                color={color}
+                            />;
+                        default:
+                            return <Ionicons 
+                                name={focused ? "alert-circle" : "alert-circle-outline"}
+                                size={size} 
+                                color={color}
+                            />;
+                    }
                 }
-            }}>
+            })}>
+            <Tabs.Screen 
+                name="Home"
+                options={{
+                    title: 'Home'
+                }}
+            />
+            <Tabs.Screen 
+                name="List"
+                options={{
+                    title: 'List'
+                }}
+            />
         </Tabs>
-    )
+    );
 }
